1c2e2e2a7dc7f1ed07ab12a541b0cc5cb196befe
Swetaketu Majumder <swetaketum@gmail.com>
2017-10-31 19:03:48 +0100

    CHANGE: Implemented app.js

---
+++ Milestone_0/app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var parser = require('body-parser');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use( express.static( "Public" ) );
+app.listen(17022);
+
+var urlencodedParser = parser.urlencoded({ extended: false })
+
+app.get('/', function(req,res)
+{
+  res.render('index');
+});
+
+app.get('/aleksandar', function(req,res)
+{
+  res.render('./Aleks/Aleks-Profile');
+});
+
+app.get('/swetaketu', function(req,res)
+{
+  res.render('./Swetaketu/index');
+});
+
+app.get('/akhila', function(req,res)
+{
+  res.render('./Akhila/Akhila_Profile');
+});
+
+app.get('/mahbubur', function(req,res)
+{
+  res.render('./Mahbub/mahbub');
+});
+
+app.get('/mahdis', function(req,res)
+{
+  res.render('./Mahdis/cv2');
+});
